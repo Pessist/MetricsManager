3996ed9b9f5142a3fb31c85375707391c16e6b58
Anton <pessist@rambler.ru>
2021-06-15 22:23:51 +0300

    Процесс выполнения домашней работы

---
+++ MetricsManager/MetricsAgent/DAL/Repository/CpuMetricsRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+using MetricsAgent.DAL.Interfaces;
+using MetricsAgent.DAL.Models;
+
+namespace MetricsAgent.DAL.Repository
+{
+    public class CpuMetricsRepository : ICpuMetricsRepository
+    {
+        private const string ConnectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100;"
+        // инжектируем соединение с базой данных в наш репозиторий через конструктор
+        public void Create(CpuMetric item)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            // создаем команду
+            using var cmd = new SQLiteCommand(connection);
+            // прописываем в команду SQL запрос на вставку данных
+            cmd.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(@value, @time)";
+
+            // добавляем параметры в запрос из нашего объекта
+            cmd.Parameters.AddWithValue("@value", item.Value);
+
+            // в таблице будем хранить время в секундах, потому преобразуем перед записью в секунды
+            // через свойство
+            cmd.Parameters.AddWithValue("@time", item.Time.ToUnixTimeSeconds);
+            // подготовка команды к выполнению
+            cmd.Prepare();
+
+            // выполнение команды
+            cmd.ExecuteNonQuery();
+
+        }
+
+        public IList<CpuMetric> GetTimeByPeriod(DateTimeOffset fromTime, DateTimeOffset toTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
